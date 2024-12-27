e1568ae279f113dd5708e8b59c05cdea2bedcdd8
zewexqa <domtui555@gmail.com>
2024-12-27 10:23:11 +0700

    Finished lab12_1.cpp

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int x);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x) {
+    if (x == 0) return 0;
+    if (x == 1) return 1;
+    return fibonacci(x - 1) + fibonacci(x - 2);
 }
