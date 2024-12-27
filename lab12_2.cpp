d484ff9b2f213fae4599d52e0f0e7dcd6edff9ed
zewexqa <domtui555@gmail.com>
2024-12-27 10:23:21 +0700

    Finished lab12_2.cpp

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 using namespace std;
-
+int gcd(int x, int y);
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+int gcd(int x, int y) {
+    if (y == 0) return x;
+    return gcd(y, x % y);
 }
